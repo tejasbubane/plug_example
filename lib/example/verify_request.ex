0911cb4efad949a1c62ae0823a28c8a59ebfd30e
Tejas Bubane <tejasbubane@gmail.com>
2018-09-17 16:33:49 +0530

    Fix request chain halting
    Return `conn` so that request does not end abruptly.

--- lib/example/verify_request.ex
+++ lib/example/verify_request.ex
@@ -4,8 +4,11 @@ defmodule Example.VerifyRequest do
   def init(options), do: options
 
   def call(%Plug.Conn{request_path: path} = conn, opts) do
-    if path in opts[:paths], do: verify_request!(conn, opts[:fields])
-    conn
+    if path in opts[:paths] do
+      verify_request!(conn, opts[:fields])
+    else
+      conn
+    end
   end
 
   defp verify_request!(conn, fields) do
@@ -14,7 +17,11 @@ defmodule Example.VerifyRequest do
       |> Map.keys()
       |> contains_fields?(fields)
 
-    unless verified, do: conn |> send_resp(400, "Incomplete Request - check all params")
+    if verified do
+      conn
+    else
+      conn |> send_resp(400, "Incomplete Request - check all params") |> halt()
+    end
   end
 
   defp contains_fields?(keys, fields), do: Enum.all?(fields, &(&1 in keys))
