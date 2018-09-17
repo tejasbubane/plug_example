06adf76ca3bd4f2d1931e3234910d9dd7a3b1a17
Tejas Bubane <tejasbubane@gmail.com>
2018-09-17 16:23:39 +0530

    Change validator to return an error message instead of throwing error

--- lib/example/verify_request.ex
+++ lib/example/verify_request.ex
@@ -1,26 +1,20 @@
 defmodule Example.VerifyRequest do
-  defmodule IncompleteRequestError do
-    @moduledoc """
-    Error raised when a required field is missing.
-    """
-
-    defexception message: "Incomplete Request - check all params", plug_status: 400
-  end
+  import Plug.Conn
 
   def init(options), do: options
 
   def call(%Plug.Conn{request_path: path} = conn, opts) do
-    if path in opts[:paths], do: verify_request!(conn.body_params, opts[:fields])
+    if path in opts[:paths], do: verify_request!(conn, opts[:fields])
     conn
   end
 
-  defp verify_request!(body_params, fields) do
+  defp verify_request!(conn, fields) do
     verified =
-      body_params
+      conn.body_params
       |> Map.keys()
       |> contains_fields?(fields)
 
-    unless verified, do: raise(IncompleteRequestError)
+    unless verified, do: conn |> send_resp(400, "Incomplete Request - check all params")
   end
 
   defp contains_fields?(keys, fields), do: Enum.all?(fields, &(&1 in keys))
